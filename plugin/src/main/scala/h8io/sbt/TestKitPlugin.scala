b66f80e1ead0a5fa4902e7d58f1e3f980ebaf4c9
Eshu <tjano.xibalba@gmail.com>
2026-04-01 20:45:18 +0900

    Publishing is fixed

--- plugin/src/main/scala/h8io/sbt/TestKitPlugin.scala
+++ plugin/src/main/scala/h8io/sbt/TestKitPlugin.scala
@@ -4,49 +4,37 @@ import sbt.*
 import sbt.Keys.*
 
 object TestKitPlugin extends AutoPlugin {
-  private def testkit: String = "testkit"
+  private val classifier: String = "testkit"
 
   override def requires: Plugins = plugins.JvmPlugin
   override def trigger: PluginTrigger = noTrigger
 
   object autoImport {
     val TestKit = config("testkit").extend(Compile).describedAs("TestKit configuration")
+    val publishTestKitArtifacts = settingKey[Boolean]("Whether to publish TestKit artifacts.")
   }
   import autoImport.*
-  val enabled: SettingKey[Boolean] = settingKey[Boolean]("Whether to publish the artifact.")
 
   override def projectConfigurations: Seq[Configuration] = Seq(TestKit)
 
   override def projectSettings: Seq[Def.Setting[?]] =
-    inConfig(TestKit)(Defaults.configSettings) ++ Seq(
-      TestKit / sourceDirectory := (ThisProject / baseDirectory).value / "src" / testkit,
-      TestKit / scalaSource := (TestKit / sourceDirectory).value / "scala",
-      TestKit / resourceDirectory := (TestKit / sourceDirectory).value / "resources",
-      Test / unmanagedSourceDirectories ++= (TestKit / unmanagedSourceDirectories).value,
-      Test / unmanagedResourceDirectories ++= (TestKit / unmanagedResourceDirectories).value,
-      TestKit / enabled := true,
-      TestKit / packageBin / artifact := (Compile / packageBin / artifact).value.withClassifier(Some(testkit)),
-      TestKit / packageSrc / artifact :=
-        (Compile / packageSrc / artifact).value.withClassifier(Some(testkit + "-sources")),
-      TestKit / packageDoc / artifact :=
-        (Compile / packageDoc / artifact).value.withClassifier(Some(testkit + "-javadoc")),
-      TestKit / artifacts ++= {
-        if ((TestKit / enabled).value)
-          Seq(
-            (TestKit / packageBin / artifact).value,
-            (TestKit / packageSrc / artifact).value,
-            (TestKit / packageDoc / artifact).value
-          )
-        else Nil
-      },
-      TestKit / packagedArtifacts ++= {
-        if ((TestKit / enabled).value)
-          Map(
-            (TestKit / packageBin / artifact).value -> (TestKit / packageBin).value,
-            (TestKit / packageSrc / artifact).value -> (TestKit / packageSrc).value,
-            (TestKit / packageDoc / artifact).value -> (TestKit / packageDoc).value
-          )
-        else Map.empty[Artifact, File]
-      }
-    )
+    inConfig(TestKit)(Defaults.configSettings) ++
+      addArtifact(TestKit / packageBin / artifact, TestKit / packageBin) ++
+      addArtifact(TestKit / packageSrc / artifact, TestKit / packageSrc) ++
+      addArtifact(TestKit / packageDoc / artifact, TestKit / packageDoc) ++
+      Seq(
+        Test / dependencyClasspath ++= (TestKit / exportedProducts).value,
+        TestKit / sourceDirectory := baseDirectory.value / "src" / classifier,
+        TestKit / scalaSource := (TestKit / sourceDirectory).value / "scala",
+        TestKit / resourceDirectory := (TestKit / sourceDirectory).value / "resources",
+        TestKit / packageBin / artifact := (Compile / packageBin / artifact).value.withClassifier(Some(classifier)),
+        TestKit / packageSrc / artifact :=
+          (Compile / packageSrc / artifact).value.withClassifier(Some(classifier + "-sources")),
+        TestKit / packageDoc / artifact :=
+          (Compile / packageDoc / artifact).value.withClassifier(Some(classifier + "-javadoc")),
+        TestKit / publishTestKitArtifacts := true,
+        TestKit / packageBin / publishArtifact := (TestKit / publishTestKitArtifacts).value,
+        TestKit / packageSrc / publishArtifact := (TestKit / publishTestKitArtifacts).value,
+        TestKit / packageDoc / publishArtifact := (TestKit / publishTestKitArtifacts).value
+      )
 }
